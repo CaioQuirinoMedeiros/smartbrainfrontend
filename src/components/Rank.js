2ba7cec44297b2dbdda28602bb7b82c6f92be818
CaioQuirinoMedeiros <48543208+CaioQuirinoMedeiros@users.noreply.github.com>
2019-05-02 19:59:08 -0300

    correcting welcome message

--- src/components/Rank.js
+++ src/components/Rank.js
@@ -3,7 +3,7 @@ import React from "react";
 const Rank = ({ name, entries }) => {
   return (
     <section className="rank">
-      <h2 className="rank_text">Wellcome, {name}</h2>
+      <h2 className="rank_text">Welcome, {name}</h2>
       <h3 className="rank_num">Images detected: {entries}</h3>
     </section>
   );
