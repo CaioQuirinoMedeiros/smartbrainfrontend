40fc63edd1aa263fddd16227775d201c89cb41da
Caio Quirino Medeiros <caio.quirino.medeiros@gmail.com>
2019-07-28 06:13:53 -0300

    Updates

---
+++ precache-manifest.8e753e9622c2948cffe09fdf33141d39.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "dbddcfec00d118350673",
+    "url": "/smartbrain_web/static/css/main.67b9031a.chunk.css"
+  },
+  {
+    "revision": "dbddcfec00d118350673",
+    "url": "/smartbrain_web/static/js/main.48eb30e8.chunk.js"
+  },
+  {
+    "revision": "bbc48ba5fe8d2c8af712",
+    "url": "/smartbrain_web/static/js/runtime~main.7345a130.js"
+  },
+  {
+    "revision": "c8061d865a2e850ab047",
+    "url": "/smartbrain_web/static/js/2.6e17266b.chunk.js"
+  },
+  {
+    "revision": "a6e129dffc2bcd5909d581a1aae9ae84",
+    "url": "/smartbrain_web/static/media/brain-solid.a6e129df.svg"
+  },
+  {
+    "revision": "c22e696df2ccba8755a71a6a6ff184b2",
+    "url": "/smartbrain_web/index.html"
+  }
+];
